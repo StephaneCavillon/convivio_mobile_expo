cd9b1a8ad9d56b235dfb14cdf6375e10247f94ad
laurieftn <laurie.ftn@gmail.com>
2021-11-23 15:27:09 +0100

    ajout de src

---
+++ src/styles/theming.js
@@ -0,0 +1,23 @@
+export const theme = {
+  colors: {
+    orange: '#F25835',
+    falseBlack: '#222222',
+    pureBlack: '#000000',
+    beige: '#F2EDE4',
+    pureWhite: '#FFFFFF',
+    falseWhite: "#F8F8F8",
+
+    text: '#222222',
+    accent: '#F25835',
+    surface: '#FFFFFF', // Couleur de background pour les éléments type cartes
+    backdrop: '#222222', // Couleur de background pour modales
+    background: '#F8F8F8',
+    placeholder:'#FFFFFF',
+    disabled: '#6E6E6E',
+    
+  },
+
+  roundness: 4, // Degré de rondeur des éléments type boutons
+
+  // Importer les fonts --> Montserrat + ses variantes regular, semiBold, bold
+}
