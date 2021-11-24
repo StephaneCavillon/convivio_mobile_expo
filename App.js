eee4c3083ae98290409c794ba6fdf30f1d1868f2
laurieftn <laurie.ftn@gmail.com>
2021-11-24 16:17:35 +0100

    interface de la homepage, sans button

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import TabNavigation from './src/navigation/TabNavigation'
+import Home from './src/screens/HomeScreen'
 
 export default function App() {
   return (
+    <View>
+      <Home />
       <TabNavigation />
+    </View>
   );
 }
