f0f12713fc09945cad3db706a3e6cb2128d3df9a
StephaneC <stephane.cavillon@itech.fr>
2021-11-24 13:52:46 +0100

    ✨ add LoginForm

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, StatusBar  } from 'react-native'
+import LoginScreen from './src/screens/loginScreen';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View >
+      <LoginScreen />
+       {/* <Text>pouet!</Text> */}
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/screens/loginScreen.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { View } from 'react-native'
+import { API } from '../utils/api'
+import LoginForm  from '../components/LoginForm'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+export default function LoginScreen () {
+  async function sendLoginRequest (credentials) {
+    const data = {
+      pseudo: credentials.pseudo,
+      password: credentials.password,
+      remember: credentials.remember
+    }
+    try {
+      const user = await API.post('/api/login', data)
+      if (user.status === 200) {
+        await registerToken(user)
+        await AsyncStorage.setItem('userId', user.data.user._id)
+        // history.push('/') // redirect to home page
+      } else {
+        console.log(user.statusText) // Status message from server response
+      }
+    } catch (error) {
+      // handle error with user not found sign up ?
+      console.log('error', error.message)
+    }
+  }
+
+  async function registerToken(user) {
+    // registering of refreshToken in localstorage
+    await AsyncStorage.setItem('accessToken', JSON.stringify(user.data.accessToken))
+  }
+
+  return (
+    <View>
+      <LoginForm sendLoginRequest={sendLoginRequest}/>
+    </View>
+  )
+}
