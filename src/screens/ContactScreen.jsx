320c762b42c1458bb5f1f4f082d5ccd1f27f8439
laurieftn <laurie.ftn@gmail.com>
2021-12-16 10:11:46 +0100

    ajustements de la page de Contact

--- src/screens/ContactScreen.jsx
+++ src/screens/ContactScreen.jsx
@@ -11,39 +11,31 @@ export default function Chat() {
         paddingHorizontal:20,
       }}>
         <View style={ {
-          paddingTop:5
+          paddingTop: 20,
         }}>
-          <Title style={{
-            textAlign:'center',
-          }} >
+          <Title style={styles.title}>
             Nous contacter
           </Title>
-          <Text style={ {
-            paddingTop: 10
-          }}>
-            Convivio, c'est une team à votre service <Text style={{ fontWeight: 'bold' }}>du lundi au samedi, de 9h00 à 18h00</Text>
+          <Text style={styles.paragraph}>
+            Convivio, c'est une team à votre service <Text style={{fontWeight: 'bold'}}>du lundi au samedi, de 9h00 à 18h00</Text>
           </Text>
-          <Text style={ {
-            paddingTop: 10
-          }}>Pour nous contacter, il suffit de nous envoyer un billet doux via cette application. Mais cela est également possible via :
+          <Text style={styles.paragraph}>Pour nous contacter, il suffit de nous envoyer un billet doux via cette application. Mais cela est également possible via :
           </Text>
         </View>
         <View style={ {
           paddingTop:20
         }}>
-          <Title style={{ paddingBottom:10 }}>Par mail</Title>
+          <Title style={styles.legend}>Par mail</Title>
           <Btn title='convivioevent@gmail.com' onPress={ () => Linking.openURL('mailto:convivioevent@gmail.com') } />
         </View>
         <View style={ {
           paddingTop:20
         }}>
-          <Title style={{ paddingBottom:10 }}>Par téléphone</Title>
-          <Btn title='03.22.22.22.22' onPress={ () => Linking.openURL('tel:0322222222') } />
+          <Title style={styles.legend}>Par téléphone</Title>
+          <Btn title='03.22.22.22.22' onPress={ () => Linking.openURL('tel: 03.22.22.22.22') } />
         </View>
-        <View style={ {
-          paddingTop:40
-        }}>
-          <Text style={{ paddingBottom:10 }}>
+        <View>
+          <Text style={styles.follow}>
             Suivez également toute l'actu de l'agence et ses projets en consultant notre site Internet:</Text>
           <Btn
             title="www.convivio.fr"
@@ -57,7 +49,7 @@ export default function Chat() {
       <Image style={{
           width: '100%',
           height: '30%',
-          top: 10,
+          top: 15,
         }}
         source={require('../assets/img/team_dessin_grey.png')}/>
     </View>
@@ -66,9 +58,27 @@ export default function Chat() {
 
 const styles = StyleSheet.create({
   title: {
-    fontWeight: 'bold',
-    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: '500',
+    fontSize: 25,
     flexDirection: 'column',
-    alignItems: 'flex-start'
+    alignItems: 'flex-start',
+    marginBottom: 10,
+  },
+
+  paragraph: {
+    paddingTop: 10,
+    fontSize: 15,
+  },
+
+  follow: {
+    fontSize: 15,
+    paddingTop: 60,
+    paddingBottom: 20,
+  },
+
+  legend: {
+    fontSize: 18,
+    paddingBottom: 10,
   }
 })
